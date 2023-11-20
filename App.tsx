65155bdd8e625777c58b31200a61aafc030fd5ca
Pad28 <mipape2281003@gmail.com>
2023-11-20 01:50:01 -0600

    env

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import 'react-native-gesture-handler';
 import { LoginStackNavigation } from './src/navigaton/LoginStackNavigation';
 
 export default function App() {
+  console.log('s');
+  
   return (
     <NavigationContainer>
       <LoginStackNavigation />
